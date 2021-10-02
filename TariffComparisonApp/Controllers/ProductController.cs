1a7497e6199164d33bb0b7d9503634af6f238032
Samson Oyetola <hello@samsonoyetola.com>
2021-10-02 17:07:58 +0100

    Added ProductController Class

--- TariffComparisonApp/Controllers/ProductController.cs
+++ TariffComparisonApp/Controllers/ProductController.cs
@@ -1,7 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace TariffComparisonApp.Controllers
 {
+    
+    [ApiController]
+    [Route("/api/v1/trariff/products")]
+    [Produces("application/json")]
     public class ProductControllers
     {
+        
+        
         
     }
 }
