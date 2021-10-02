a7ba6d4e27a1af42afa670edaecda758fdef90b8
Samson Oyetola <hello@samsonoyetola.com>
2021-10-02 17:02:41 +0100

    Added UnitTest to Compare

--- TariffComparisonApp.Tests/TariffComparisonTest.cs
+++ TariffComparisonApp.Tests/TariffComparisonTest.cs
@@ -12,6 +12,27 @@ namespace TariffComparisonApp.Tests
         public TariffComparisonTest(ITestOutputHelper testOutputHelper) : base(testOutputHelper) { }
         
         
+        [Fact]
+        public async Task CompareTest()
+        {
+            // Arrange
+            await SeedDataAsync();
+            var sss = await DatabaseContext.Products.FirstOrDefaultAsync();
+
+            // Act
+            var products = DatabaseContext.Products.AsEnumerable().Select(p=>new ProductDto
+            {
+                TariffName = p.TariffName,
+                AnnualCosts = p.PriceCalculation.GetAnnualCosts(3500),
+            }).OrderBy(p=>p.AnnualCosts);
+
+            // Assert
+            Assert.Equal(2, products.Count());
+            Assert.True(products.First().AnnualCosts <= products.Last().AnnualCosts);
+        }
+
+        
+
 
 
 
